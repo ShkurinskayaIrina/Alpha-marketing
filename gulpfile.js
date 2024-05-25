2a65f25042a3aad134b4a84c1e1ce76ad3300783
ShkurinskayaIrina <shkurinskayaIrina@gmail.com>
2024-05-26 01:44:34 +0300

    Настроен gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,63 @@
 import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
 import plumber from 'gulp-plumber';
+import sourcemap from 'gulp-sourcemaps';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import { deleteAsync } from 'del';
 import browser from 'browser-sync';
 
 // Styles
-
-export const styles = () => {
-  return gulp.src('sass/style.scss', { sourcemaps: true })
+const styles = () => {
+  return gulp.src("sass/style.scss")
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemap.init())
+    .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('css', { sourcemaps: '.' }))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(browser.stream());
 }
 
-// Server
+// HTML
+const html = () => {
+  return gulp.src("*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
 
+//Images
+const copyImages = () => {
+  return gulp.src("img/**/*{jpg,png,svg,webp}")
+  .pipe(gulp.dest("build/img"));
+}
+
+// Copy
+const copy = (done) => {
+  gulp.src([
+    "*fonts/**/*.{woff2,woff}"
+  ])
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+// Clean
+const clean = () => {
+  return deleteAsync("build");
+};
+
+// Server
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: './'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -32,14 +66,39 @@ const server = (done) => {
   done();
 }
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch('sass/**/*.scss', gulp.series(styles));
-  gulp.watch('*.html').on('change', browser.reload);
+// Reload
+const reload = (done) => {
+  browser.reload();
+  done();
 }
 
+// Watcher
+const watcher = () => {
+  gulp.watch("sass/**/*.scss", gulp.series(styles));
+  gulp.watch("*.html", gulp.series(html, reload));
+}
 
-export default gulp.series(
-  styles, server, watcher
+// Build
+export const build = gulp.series (
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel (
+    styles,
+    html
+  ),
 );
+
+// Default
+export default gulp.series (
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel (
+    styles,
+    html
+  ),
+  gulp.series (
+    server,
+    watcher
+  ));
